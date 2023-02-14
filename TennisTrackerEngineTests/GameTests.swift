2659ee6a3288969fd1ce81e9d0ef651000d71f38
David Rivera <davidrivera.dev@gmail.com>
2023-02-13 21:08:22 -0500

    Removed file references from UITest target. Added @testable import

--- TennisTrackerEngineTests/GameTests.swift
+++ TennisTrackerEngineTests/GameTests.swift
@@ -1,5 +1,6 @@
 import Foundation
 import XCTest
+@testable import TennisTrackerEngine
 
 class GameTests: XCTestCase {
 
--- TennisTrackerEngineTests/MatchTests.swift
+++ TennisTrackerEngineTests/MatchTests.swift
@@ -1,5 +1,6 @@
 import Foundation
 import XCTest
+@testable import TennisTrackerEngine
 
 class MatchTests: XCTestCase {
 
